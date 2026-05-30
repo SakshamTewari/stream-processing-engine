dd9a92bf5206f5e461c41fc39657deecd21b6aed
Saksham Tewari <sakshamtewari3@gmail.com>
2026-05-31 00:32:13 +0530

    update: DLQ and metrics routes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import {router} from './api/event.routes';
+import {deadLetterRouter} from "./api/dead-letter.routes";
+import {metricsRouter} from "./api/metrics.route";
 import { startWorker } from './workers/worker';
+
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', router);
+app.use('/api/events', router);
+app.use('/api/dead-letters', deadLetterRouter);
+app.use('/api/metrics', metricsRouter);
 
 
 /*
