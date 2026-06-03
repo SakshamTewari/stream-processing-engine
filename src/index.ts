598962811d206c35b61f882d4354cade0791751d
Saksham Tewari <sakshamtewari3@gmail.com>
2026-06-03 21:37:46 +0530

    update: call recoveryService before startWorker

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import {router} from './api/event.routes';
 import {deadLetterRouter} from "./api/dead-letter.routes";
 import {metricsRouter} from "./api/metrics.route";
 import { startWorker } from './workers/worker';
+import {healthRouter} from "./api/health.route";
+import {recoveryService} from "./recovery/recovery.service";
 
 
 const PORT = 3000;
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use('/api/events', router);
 app.use('/api/dead-letters', deadLetterRouter);
 app.use('/api/metrics', metricsRouter);
+app.use('/api/health', healthRouter);
 
 
 /*
@@ -21,8 +24,11 @@ continue execution
 run callbacks later
 */
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`); 
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+    
+    // Recover events from previous runs and enqueue them for processing first
+    await recoveryService.recover();
     startWorker();
 });
 
