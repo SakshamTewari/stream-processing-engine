a96adf9bb7439e01ac2445d6bedc2472b553b259
Saksham Tewari <sakshamtewari3@gmail.com>
2026-06-07 01:04:15 +0530

    update: getAll() return type

--- src/event-store/event-store.interface.ts
+++ src/event-store/event-store.interface.ts
@@ -12,9 +12,13 @@ Event Store Cleanup
 
 
 import { BaseEvent } from "../events/event.types";
+import { StoredEvent } from "./event-store.types";
 
 export interface EventStore {
     save(event: BaseEvent) : Promise<void>;
-    getAll(): Promise<BaseEvent[]>;
-    remove(eventId: string): Promise<void>;
+    getAll(): Promise<StoredEvent[]>;
+    markProcessing(eventId: string): Promise<void>;
+    markCompleted(eventId: string): Promise<void>;
+    markFailed(eventId: string): Promise<void>;
+    // remove(eventId: string): Promise<void>;    not needed now as we have EventStatus now
 }
