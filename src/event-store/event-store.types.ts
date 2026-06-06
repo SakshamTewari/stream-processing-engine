63ba7fd52cb58bc3de3a40cdcfa88c8b02ab14e2
Saksham Tewari <sakshamtewari3@gmail.com>
2026-06-07 01:02:29 +0530

    add: StoredEvent interface

---
+++ src/event-store/event-store.types.ts
@@ -0,0 +1,10 @@
+import {BaseEvent} from "../events/event.types";
+import {EventStatus} from "../events/event-status.types";
+
+export interface StoredEvent {
+    event: BaseEvent;
+    status: EventStatus;
+    storedAt: number;
+    claimedAt?: number;
+    completedAt?: number;
+}
