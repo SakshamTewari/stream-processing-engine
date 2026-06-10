034819fc095eec2b7f40fb113cd838373e2511e3
Saksham Tewari <sakshamtewari3@gmail.com>
2026-06-10 22:15:14 +0530

    update: recover staleProcessingEvents

--- src/recovery/recovery.service.ts
+++ src/recovery/recovery.service.ts
@@ -24,10 +24,19 @@ export class RecoveryService {
         const events = await eventStore.getAll();
 
         const pendingEvents = events.filter(e => e.status === 'PENDING');
+        const staleProcessingEvents = await eventStore.getStaleProcessingEvents();
 
         for(const storedEvent of pendingEvents){
             queue.enqueue(storedEvent.event);
-        }
+        };
+
+        for(const storedEvent of staleProcessingEvents){
+            await eventStore.markPending(storedEvent.event.id);
+            queue.enqueue(storedEvent.event);
+            loggingService.warn(LOG_COMPONENTS.RECOVERY,`Reclaimed Stale Processing Event`, {staleProcessingEvent: storedEvent.event.id, eventName: storedEvent.event.payload.title});
+        };
+
+        
         loggingService.info(LOG_COMPONENTS.RECOVERY,`Recovery Completed`, {recoveredEvents: pendingEvents.length});
     }
 }
