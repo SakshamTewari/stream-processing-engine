2cb297d97b4fb5f68e62afa235c59d4aa893c56a
Saksham Tewari <sakshamtewari3@gmail.com>
2026-06-23 21:52:11 +0530

    update: isFull, enqueue -->boolean

--- src/queue/in-memory.queue.ts
+++ src/queue/in-memory.queue.ts
@@ -3,17 +3,26 @@ import type {Queue} from './queue.interface';
 
 export class InMemoryQueue implements Queue{
     private queue: BaseEvent[] = [];
+    private readonly maxSize: number = 1000;
 
-    enqueue(event: BaseEvent){
+    enqueue(event: BaseEvent): boolean{
+        if (this.queue.length >= this.maxSize) {
+            return false;
+        }
         this.queue.push(event);
-    }
+        return true;
+    };
 
     dequeue(): BaseEvent | undefined {
         return this.queue.shift();
-    }
+    };
 
     size(): number {
         return this.queue.length;
+    };
+
+    isFull(): boolean {
+        return this.queue.length >= this.maxSize;
     }
 }
 
--- src/queue/queue.interface.ts
+++ src/queue/queue.interface.ts
@@ -1,7 +1,8 @@
 import type {BaseEvent} from "../events/event.types";
 
 export interface Queue {
-    enqueue(event: BaseEvent): void;
+    enqueue(event: BaseEvent): boolean;
     dequeue(): BaseEvent | undefined;
     size(): number;
+    isFull(): boolean;
 }
