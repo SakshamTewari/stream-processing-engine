5ca5f84ce3ae39e7f7c58bd06d1cc1e25e7750ec
Saksham Tewari <sakshamtewari3@gmail.com>
2026-06-25 21:52:05 +0530

    update: migrate to MetricsStore class

--- src/metrics/metrics.store.ts
+++ src/metrics/metrics.store.ts
@@ -1,5 +1,17 @@
-export const metrics = {
-    processed: 0,
-    failed: 0,
-    retried: 0,
+import { Metrics } from "./metrics.types";
+
+export class MetricsStore  {
+    private metrics: Metrics = {
+        processed: 0,
+        failed: 0,
+        retried: 0,
+        deadLettered: 0,
+        rejected: 0
+    };
+
+    getMetrics(): Metrics {
+        return this.metrics;
+    };
 }
+
+export const metricsStore = new MetricsStore();
